a39a4dce2bee053e16339ec2e7c5e57db2bb293a
Cipher163 <122257078+Cipher163@users.noreply.github.com>
2026-06-29 19:15:43 +0800

    Delete OMS_Refactored directory

--- OMS_Refactored/src/oms/Bicycle.java
+++
@@ -1,9 +0,0 @@
-package oms;
-
-/** F8: a Bicycle does NOT implement Engine, so it has no startEngine() to misuse. */
-public final class Bicycle extends Vehicle {
-    @Override
-    public void move() {
-        System.out.println("Bicycle is pedalling");
-    }
-}
